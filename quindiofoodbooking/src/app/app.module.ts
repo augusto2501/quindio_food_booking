07cb1588dee4fedfd068e6ecd64ff3bf860858c1
Juliana Leal <julianyta0325@hotmail.com>
2021-09-08 16:13:34 -0500

    componentes nav e inicio

--- quindiofoodbooking/src/app/app.module.ts
+++ quindiofoodbooking/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { InitComponent } from './components/init/init.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    InitComponent
   ],
   imports: [
     BrowserModule,
